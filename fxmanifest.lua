edd3cced465f55e321a9bb01ed5feff131afa782
LG <158826906+lundgreen01@users.noreply.github.com>
2025-02-02 18:28:44 +0100

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version "cerulean"
+game "gta5"
+
+client_scripts {
+    "client/main.lua"
+}
+
+files {
+  "html/index.html",
+  "html/assets/*.js",
+  "html/assets/*.css"
+}
+
+ui_page "html/index.html"
+                  
